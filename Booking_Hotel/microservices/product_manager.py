83fe7a34226f360868598fe5aa3a81a473134bd4
Wen Hai Oh <wenhai.oh.2021@scis.smu.edu.sg>
2023-03-11 14:46:11 +0800

    Create product_manager.py
    to create local "product_manager" database and table. Tested working to get all products and individual product by productID.

---
+++ Booking_Hotel/microservices/product_manager.py
@@ -0,0 +1,77 @@
+from flask import Flask, request, jsonify
+from flask_sqlalchemy import SQLAlchemy
+from flask_cors import CORS
+
+# Product Manager (productID, productName, images, productRate)
+
+app = Flask(__name__)
+app.config["SQLALCHEMY_DATABASE_URI"] = "mysql+mysqlconnector://root@localhost:3306/product_manager"
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+
+db = SQLAlchemy(app)
+CORS(app)
+
+class ProductManager(db.Model):
+    __tablename__ = "product_manager"
+
+    productID = db.Column(db.Integer, primary_key=True)
+    productName = db.Column(db.String(64), nullable=False)
+    images = db.Column(db.String(64), nullable=False)
+    productRate = db.Column(db.String(64), nullable=False)
+
+    def __init__(self, productID, productName, images, productRate):
+        self.productID = productID
+        self.productName = productName
+        self.images = images
+        self.productRate = productRate
+
+    def json(self):
+        return {
+            "productID": self.productID,
+            "productName": self.productName,
+            "images": self.images,
+            "productRate": self.productRate,
+        }
+
+# Get all products.
+# Returns in the format of JSON.
+@app.route("/product")
+def get_all_product():
+    productList = ProductManager.query.all()
+    if len(productList):
+        return jsonify(
+            {
+                "code": 200,
+                "data": {
+                    "product": [product.json() for product in productList]
+                }
+            }
+        )
+    return jsonify(
+        {
+            "code": 404,
+            "message": "There are no products."
+        }
+    ), 404
+
+# Get product by productID.
+# Returns in the format of JSON.
+@app.route("/product/<int:productID>")
+def find_by_productID(productID):
+    product = ProductManager.query.filter_by(productID=productID).first()
+    if product:
+        return jsonify(
+            {
+                "code": 200,
+                "data": product.json()
+            }
+        )
+    return jsonify(
+        {
+            "code": 404,
+            "message": "Product with ID {} not found.".format(productID)
+        }
+    ), 404
+
+if __name__ == '__main__':
+    app.run(port=5000, debug=True)
