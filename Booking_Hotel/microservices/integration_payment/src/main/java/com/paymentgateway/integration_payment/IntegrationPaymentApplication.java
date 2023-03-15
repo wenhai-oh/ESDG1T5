b8957a057d661eea794e99adde9a8e4f3773d0ae
Waisoon123 <waisoon1999@gmail.com>
2023-03-15 16:38:37 +0800

    Create Spring Boot Project for Paypal Integration with Java
    Updated pom.xml with paypal dependencies.

---
+++ Booking_Hotel/microservices/integration_payment/src/main/java/com/paymentgateway/integration_payment/IntegrationPaymentApplication.java
@@ -0,0 +1,13 @@
+package com.paymentgateway.integration_payment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IntegrationPaymentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IntegrationPaymentApplication.class, args);
+	}
+
+}
---
+++ Booking_Hotel/microservices/integration_payment/src/test/java/com/paymentgateway/integration_payment/IntegrationPaymentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.paymentgateway.integration_payment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IntegrationPaymentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
