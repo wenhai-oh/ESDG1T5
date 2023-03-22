ea4382c1cef7f9fd58e10dd33e77e3b7263185f5
Waisoon123 <waisoon1999@gmail.com>
2023-03-22 13:54:47 +0800

    Create OrderDetail.java
    This class provides the getter functions for us to return Strings from PayPal API.

---
+++ Booking_Hotel/microservices/Paypal_Integration/paypal_integration/src/main/webapp/OrderDetail.java
@@ -0,0 +1,36 @@
+package com.esdg1t5;
+
+public class OrderDetail{
+    private String roomName;
+    private float subTotal;
+    private float tax;
+    private float total;
+
+    public OrderDetail(String roomName, float subTotal, float tax, float total) {
+        suoer();
+        this.roomName = roomName;
+        this.subTotal = subTotal;
+        this.tax = tax;
+        this.total = total;
+    }
+
+    // Getters Functions for the above
+    // Note that the getter methods return String for currency values because PayPal API requires the amount in String.
+
+    public String getRoomName() {
+        return roomName;
+    }
+
+    public String getSubTotal() {
+        return String.format("%.2f", subTotal);
+    }
+
+    public String getTax() {
+        return String.format("%.2f", tax);
+    }
+
+    public string getTotal() {
+        return String.format("%.2f", total);
+    }
+
+}
