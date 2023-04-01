649e31e9ec23825f267f1797caa777d1fabdf15f
Waisoon123 <waisoon1999@gmail.com>
2023-04-01 08:55:29 +0800

    Update refund.py and thanks.html
    Tested on postman. Thanks.html UI is working as of now.

--- Booking_Hotel/microservices/flask_stripe/flask_stripe/refund.py
+++ Booking_Hotel/microservices/flask_stripe/flask_stripe/refund.py
@@ -6,20 +6,23 @@ stripe.api_key = 'sk_test_51Mp7eaDR3XsfzYNcNN274W7Hmlsuvr7nDhtFN1UtuVH9dHVBRVVC9
 
 @app.route('/refund', methods=['POST'])
 def create_refund():
-    session_id = request.json['session_id']
-    
-    # Retrieve the session
-    session = stripe.checkout.Session.retrieve(session_id)
-    
-    # Get the payment intent ID from the session
-    payment_intent_id = session['payment_intent']
-    
-    # Create the refund
-    refund = stripe.Refund.create(
-        payment_intent=payment_intent_id
-    )
-    
-    return jsonify({'message': 'Refund Successful!'}), 200
+    try:
+        session_id = request.json['session_id']
+        
+        # Retrieve the session
+        session = stripe.checkout.Session.retrieve(session_id)
+        
+        # Get the payment intent ID from the session
+        payment_intent_id = session['payment_intent']
+        
+        # Create the refund
+        refund = stripe.Refund.create(
+            payment_intent=payment_intent_id
+        )
+        
+        return jsonify({'message': 'Refund Successful!'}), 200
+    except:
+        return 'Unsuccesful Refund due to reasons'
 
 if __name__ == '__main__':
-    app.run(port=5001, debug=True)
+    app.run(port=5002, debug=True)
